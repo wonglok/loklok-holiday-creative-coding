4e85714643370991f2d8ffa43c58d7d577f000b5
Wong Lok <wonglok@users.noreply.github.com>
2023-09-09 20:52:53 +0800

    fa

--- src/components/FancyTouchCircle/Rain/FunSim.js
+++ src/components/FancyTouchCircle/Rain/FunSim.js
@@ -427,9 +427,9 @@ export class LokLokWiggleDisplay {
 
           float tt = (1.0 - vT);
 
-          vec3 color = pal(tt + time, vec3(0.0,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,0.5),vec3(0.8,0.90,0.30));
+          vec3 color = pal(tt * vLineCycle + time, vec3(0.0,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,0.5),vec3(0.8,0.90,0.30));
 
-          gl_FragColor = vec4(matcapColor.rgb * 0.0 + color, tt * vLineCycle);
+          gl_FragColor = vec4(color, tt * vLineCycle);
 
           if (vLineCycle >= 0.0 && vLineCycle <= 0.333) {
             gl_FragColor.x *= 1.0 * tt;
