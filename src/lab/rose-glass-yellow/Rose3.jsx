f8f0ab9f6b77a71d4f6ac73f1336f7311e698d99
Wong Lok <wonglok@users.noreply.github.com>
2023-09-25 10:30:03 +0800

    faf

--- src/lab/rose-glass-yellow/Rose3.jsx
+++ src/lab/rose-glass-yellow/Rose3.jsx
@@ -20,92 +20,6 @@ import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler'
 export function Rose3({ ...props }) {
   const { nodes, materials } = useGLTF('/rose/rose3.glb')
 
-  let uniforms = useMemo(() => {
-    return {
-      time: { value: 0 },
-    }
-  }, [])
-  let { envMap: rose3Env } = useComputeEnvMap(
-    /* glsl */ `
-    const mat2 m = mat2( 0.80,  0.60, -0.60,  0.80 );
-
-    float noise( in vec2 p ) {
-      return sin(p.x)*sin(p.y);
-    }
-
-    float fbm4( vec2 p ) {
-        float f = 0.0;
-        f += 0.5000 * noise( p ); p = m * p * 2.02;
-        f += 0.2500 * noise( p ); p = m * p * 2.03;
-        f += 0.1250 * noise( p ); p = m * p * 2.01;
-        f += 0.0625 * noise( p );
-        return f / 0.9375;
-    }
-
-    float fbm6( vec2 p ) {
-        float f = 0.0;
-        f += 0.500000*(0.5 + 0.5 * noise( p )); p = m*p*2.02;
-        f += 0.250000*(0.5 + 0.5 * noise( p )); p = m*p*2.03;
-        f += 0.125000*(0.5 + 0.5 * noise( p )); p = m*p*2.01;
-        f += 0.062500*(0.5 + 0.5 * noise( p )); p = m*p*2.04;
-        f += 0.031250*(0.5 + 0.5 * noise( p )); p = m*p*2.01;
-        f += 0.015625*(0.5 + 0.5 * noise( p ));
-        return f/0.96875;
-    }
-
-    float pattern (vec2 p, float time) {
-      float vout = fbm4( p + time + fbm6(  p + fbm4( p + time )) );
-      return abs(vout);
-    }
-
-    uniform sampler2D hdrTexture;
-    varying vec3 vWorldDirection;
-    varying vec3 vPos;
-    #define RECIPROCAL_PI 0.31830988618
-    #define RECIPROCAL_PI2 0.15915494
-
-    uniform float time;
-    uniform float rotY;
-
-    mat3 rotateY(float rad) {
-        float c = cos(rad);
-        float s = sin(rad);
-        return mat3(
-            c, 0.0, -s,
-            0.0, 1.0, 0.0,
-            s, 0.0, c
-        );
-    }
-
-    vec4 mainImage ()  {
-      vec3 direction = normalize( vWorldDirection * rotateY(time * 0.0165));
-      vec2 uv;
-      uv.y = asin( clamp( direction.y, - 1.0, 1.0 ) ) * RECIPROCAL_PI + 0.5;
-      uv.x = atan( direction.z, direction.x ) * RECIPROCAL_PI2 + 0.5;
-
-      // vec4 hdrTextureC4 = texture2D(hdrTexture, uv);
-
-      vec4 outColor;
-      outColor.a = 1.0;
-
-      float pLayout1 = pow(pattern(time * 0.5 + uv.xy * 5.0 + 0.01, 0.1 * time), 1.3);
-
-      outColor.rgb = vec3(
-        pLayout1 * 0.55 + 0.05 + 0.5,
-        pLayout1 * 0.1 + 0.05,
-        pLayout1 * 0.1 + 0.05
-      );
-
-      outColor.r = pow(outColor.r, 2.0);
-      
-      return outColor;
-    }
-
-  `,
-    uniforms,
-    64,
-  )
-
   return (
     <>
       <group scale={0.1} rotation={[-0.25 * Math.PI, 0, 0]} position={[0, 0.2, 0]} {...props} dispose={null}>
@@ -113,16 +27,15 @@ export function Rose3({ ...props }) {
           <group position={[20.76555, 6.82571, 1.82089]} rotation={[0, 0, 1.32211]} scale={0.13645}>
             {nodes.petals && (
               <mesh name='petals' castShadow receiveShadow geometry={nodes.petals.geometry}>
-                {rose3Env && (
+                {
                   <meshPhysicalMaterial
-                    metalness={1.0}
-                    envMapIntensity={0.5}
-                    envMap={rose3Env}
+                    metalness={0.8}
+                    envMapIntensity={0.56}
                     side={DoubleSide}
                     roughness={0.1}
-                    color={'#ff0'}
+                    color={'#990'}
                   />
-                )}
+                }
               </mesh>
             )}
             <Particles nodes={nodes}></Particles>
